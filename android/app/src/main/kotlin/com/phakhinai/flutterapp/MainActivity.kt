00f1d5ade3c60f1d8db5f527a31a094581c9ea79
PHAKHINAI MAISUK <m.phakhinai@gmail.com>
2022-04-09 16:58:56 +0700

    Initial project flutter

---
+++ android/app/src/main/kotlin/com/phakhinai/flutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.phakhinai.flutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/src/app.dart';
+
+void main() {
+  runApp(const App());
+}
